406d0bc8656903ddcee1f5e7f5ac5b268fadbdb9
omar <omaraser93@gmail.com>
2020-10-17 16:39:02 +0200

    Updates

---
+++ precache-manifest.4b50b57531e91777d58c3a773bad3d07.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf3dd7eb23b6fdfcf7ecd211977bcd2a",
+    "url": "/Books-List/index.html"
+  },
+  {
+    "revision": "96d07db378a7f9256162",
+    "url": "/Books-List/static/css/main.36950ceb.chunk.css"
+  },
+  {
+    "revision": "2fc64ae0b238b4a51b00",
+    "url": "/Books-List/static/js/2.b48c095d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Books-List/static/js/2.b48c095d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96d07db378a7f9256162",
+    "url": "/Books-List/static/js/main.c4a68fd0.chunk.js"
+  },
+  {
+    "revision": "c671513daf1a8f459b42",
+    "url": "/Books-List/static/js/runtime-main.3f3da344.js"
+  },
+  {
+    "revision": "f83b8138b71e120aeb3c43fec207076a",
+    "url": "/Books-List/static/media/library-img.f83b8138.jpeg"
+  }
+]);
